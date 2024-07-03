c4ded39a4ef84bb7c4d713a94a20fc9d411b656e
Angelo <50700612+baki2211@users.noreply.github.com>
2024-07-03 19:34:54 +0100

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,10 +2,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$hostname = "127.0.0.1";
-$username = "root"; 
+$hostname = "";
+$username = ""; 
 $password = "";
-$dbname = ""; 
+$dbname = "mediaLybrary"; 
 
 $connect = new mysqli($hostname, $username, $password, $dbname);
 
